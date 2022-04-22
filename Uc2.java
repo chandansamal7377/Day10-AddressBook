54fdfa4ff687273167addbafe89c3f08dbcf42d1
Chandan Samal <chandansamal9800@gmail.com>
2022-04-23 00:28:03 +0530

    Added: add a new contact in address book

---
+++ Uc2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Uc2 {
+    public static void main(String arg[]) {
+        ContactDetails person;
+        Scanner sc =new Scanner(System.in);
+}
+        public void add() {
+            person = new ContactDetails();
+
+       System.out.println("Enter First Name: ");
+       String fname = sc.nextLine();
+
+       System.out.println("Enter Last Name: ");
+        String lname = sc.nextLine();
+
+         System.out.println("Enter Address: ");
+         String address = sc.nextLine();
+
+        System.out.println("Enter City: ");
+        String city = sc.nextLine();
+
+        System.out.println("Enter State: ");
+        String state = sc.nextLine();
+
+        System.out.println("Enter Your Zip: ");
+        String zip = sc.nextLine();
+
+        System.out.println("Enter Your MobileNumber: ");
+        String Mobile = sc.nextLine();
+
+         System.out.println("Enter Your Email: ");
+         String email = sc.nextLine();
+            person.setFirstName(fname);
+            person.setLastName(lname);
+            person.setAddress(address);
+            person.setCity(city);
+            person.setState(state);
+            person.setZipCode(zip);
+            person.setPhoneNumber(Mobile);
+            person.setEmail(email);
+
+            System.out.println(person);
+
+        }
+    }
